3c8ad67f65147463a5f5904ec3ad2cfdf00039c6
Ryan Bagwell <ryan@ryanbagwell.com>
2012-07-18 22:31:27 -0500

    adding plugin

---
+++ optionspage.html.php
@@ -0,0 +1,45 @@
+<div class="wrap">
+	<h2>Sentry Error Reporting Settings</h2>
+	
+	<form action="" method="post">
+		
+		<table class="form-table">
+			<tbody>
+				<tr valign="top">
+					<th scope="row">
+						<label for="sentry_dsn">Sentry DSN</label>
+					</th>
+					<td>
+						<input name="sentry_dsn" type="text" id="sentry-dsn" value="<?php echo $dsn; ?>" class="regular-text">
+					</td>
+				</tr>
+				
+				<tr valign="top">
+					<th scope="row">
+						<label for="sentry_dsn">Error Reporting Level</label>
+					</th>
+					<td>
+						<select name="sentry_reporting_level">
+							<option value="E_NONE" <?php echo ($reporting_level == 'E_NONE') ? 'selected="selected"' : '';?>>None</option>
+							<option value="E_WARNING" <?php echo ($reporting_level == 'E_WARNING') ? 'selected="selected"' : '';?>>Warnings</option>
+							<option value="E_NOTICE" <?php echo ($reporting_level == 'E_NOTICE') ? 'selected="selected"' : '';?>>Notices</option>
+							<option value="E_USER_ERROR" <?php echo ($reporting_level == 'E_USER_ERROR') ? 'selected="selected"' : '';?>>User Errors</option>
+							<option value="E_USER_WARNING" <?php echo ($reporting_level == 'E_USER_WARNING') ? 'selected="selected"' : '';?>>User Warnings</option>
+							<option value="E_USER_NOTICE" <?php echo ($reporting_level == 'E_USER_NOTICE') ? 'selected="selected"' : '';?>>User Notices</option>
+							<option value="E_RECOVERABLE_ERROR" <?php echo ($reporting_level == 'E_RECOVERABLE_ERROR') ? 'selected="selected"' : '';?>>Recoverable Errors</option>
+							<option value="E_ALL" <?php echo ($reporting_level == 'E_ALL') ? 'selected="selected"' : '';?>>All Errors</option>
+						</select>
+					</td>
+				</tr>				
+			</tbody>
+		</table>
+		
+		<p class="submit">
+			<input type="submit" name="submit" id="submit" class="button-primary" value="Save Changes">
+		</p>
+		
+		<?php var_dump( $this->settings ); ?>
+
+	</form>
+	
+</div>
---
+++ wordpress-sentry.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Plugin Name: WordPress Sentry Client
+Plugin URI: http://www.hzdg.com
+Description: Sends PHP errors to Django Sentry
+Author: Ryan Bagwell
+Version: 1
+Author URI: http://www.ryanbagwell.com
+*/
+
+require_once( dirname(__FILE__).'/raven/lib/Raven/Autoloader.php' );
+Raven_Autoloader::register();
+
+class WPSentry extends Raven_Client {
+	
+	function WPSentry() {
+		
+		if ( $_POST ) $this->saveOptions();
+		
+		add_action( 'admin_menu', array( $this, 'addOptionsPage' ));
+		
+		$this->settings = get_option('sentry-settings');
+		
+		$this->setErrorReportingLevel( $this->max_error_reporting_level );
+		
+		if ( !$this->dsn ) return;
+		
+		parent::__construct( $this->dsn );
+		
+		$this->setHandlers();
+		
+	}
+	
+	function setHandlers() {
+		$error_handler = new Raven_ErrorHandler( $this );
+		set_error_handler( array( $error_handler, 'handleError' ));
+		set_exception_handler( array( $error_handler, 'handleException' ));
+	}
+	
+	function setErrorReportingLevel( $level = 'E_WARNING' ) {
+		
+		if ( isset( $this->settings['reporting_level'] ) )
+			$level = $this->settings['reporting_level'];
+		
+		$errorLevelMap = array(
+			'E_NONE' => 0,
+			'E_WARNING' => 2,
+			'E_NOTICE' => 8,
+			'E_USER_ERROR' => 256,
+			'E_USER_WARNING' => 512,
+			'E_USER_NOTICE' => 1024,
+			'E_RECOVERABLE_ERROR' => 4096,
+			'E_ALL' => 8191
+			);
+		
+		if ( array_key_exists( $level, $errorLevelMap ) )
+			$this->_max_error_reporting_level = $errorLevelMap[ $level ];
+			
+	}
+	
+	function addOptionsPage() {
+		add_options_page('Sentry Error Reporting Settings', 'Sentry', 8, 'sentrysettings', array( $this, 'printOptionsHTML' ));
+	}
+	
+	function printOptionsHTML() {		
+		extract( $this->settings );
+		require_once( dirname(__FILE__).'/optionspage.html.php' );	
+	}
+	
+	function saveOptions() {
+		
+		if ( !isset( $_POST[ 'sentry_dsn' ] ) || !isset( $_POST[ 'sentry_reporting_level' ]))
+			return;
+		
+		update_option('sentry-settings', array(
+			'dsn' => $_POST[ 'sentry_dsn' ],
+			'reporting_level' => $_POST[ 'sentry_reporting_level' ]
+		));
+
+	}
+
+}
+
+add_action('init', create_function(null, '$wps = new WPSentry(); ') );
