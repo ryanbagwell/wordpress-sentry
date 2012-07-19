9889f35c772366a3209c87f0ca68d27e2d23dbba
Ryan Bagwell <ryan@ryanbagwell.com>
2012-07-19 14:30:56 -0500

    making a new class for wp-specific raven client; loading all classes up front to work around bug in auto loader

---
+++ class.wp-raven-client.php
@@ -0,0 +1,54 @@
+<?php
+
+
+require_once( dirname(__FILE__).'/raven/lib/Raven/Client.php' );
+require_once( dirname(__FILE__).'/raven/lib/Raven/ErrorHandler.php' );
+require_once( dirname(__FILE__).'/raven/lib/Raven/StackTrace.php' );
+
+class WP_Raven_Client extends Raven_Client {
+	
+	function __construct() {
+		
+		$this->settings = get_option( 'sentry-settings' );
+		
+		if ( !isset( $this->settings['dsn'] )) 
+			return;
+		
+		parent::__construct( $this->settings['dsn'] );
+		
+		$this->setErrorReportingLevel( $this->settings['reporting_level'] );
+		
+		$this->setHandlers();
+		
+	}
+	
+	function setHandlers() {
+		$error_handler = new Raven_ErrorHandler( $this );
+		set_error_handler( array( $error_handler, 'handleError' ));
+		set_exception_handler( array( $error_handler, 'handleException' ));
+	}
+	
+	function setErrorReportingLevel( $level = 'E_WARNING' ) {
+		
+		
+		
+		$errorLevelMap = array(
+			'E_NONE' => 0,
+			'E_WARNING' => 2,
+			'E_NOTICE' => 8,
+			'E_USER_ERROR' => 256,
+			'E_USER_WARNING' => 512,
+			'E_USER_NOTICE' => 1024,
+			'E_RECOVERABLE_ERROR' => 4096,
+			'E_ALL' => 8191
+			);
+		
+		if ( array_key_exists( $level, $errorLevelMap ) )
+			$this->_max_error_reporting_level = $errorLevelMap[ $level ];
+	}	
+	
+	
+}
+
+
+?> 
--- wordpress-sentry.php
+++ wordpress-sentry.php
@@ -7,51 +7,21 @@ Author: Ryan Bagwell
 Version: 1
 Author URI: http://www.ryanbagwell.com
 */
-
 require_once( dirname(__FILE__).'/raven/lib/Raven/Autoloader.php' );
 Raven_Autoloader::register();
+require_once( dirname(__FILE__).'/class.wp-raven-client.php' );
 
-class WPSentry extends Raven_Client {
+class WPSentry extends WP_Raven_Client {
 	
 	function WPSentry() {
-				
-		if ( is_admin() && $_POST ) $this->saveOptions();
+
 		add_action( 'admin_menu', array( $this, 'addOptionsPage' ));
-		$this->settings = get_option('sentry-settings');
-		$this->setErrorReportingLevel();
 				
-		if ( !isset($this->settings['dsn']) ) return;
+		if ( is_admin() && $_POST )
+			$this->saveOptions();
 		
-		parent::__construct( $this->settings['dsn'] );
+		parent::__construct();
 		
-		$this->setHandlers();
-		
-	}
-	
-	function setHandlers() {
-		$error_handler = new Raven_ErrorHandler( $this );
-		set_error_handler( array( $error_handler, 'handleError' ));
-		set_exception_handler( array( $error_handler, 'handleException' ));
-	}
-	
-	function setErrorReportingLevel( $level = 'E_WARNING' ) {
-		
-		if ( isset( $this->settings['reporting_level'] ) )
-			$level = $this->settings['reporting_level'];
-		
-		$errorLevelMap = array(
-			'E_NONE' => 0,
-			'E_WARNING' => 2,
-			'E_NOTICE' => 8,
-			'E_USER_ERROR' => 256,
-			'E_USER_WARNING' => 512,
-			'E_USER_NOTICE' => 1024,
-			'E_RECOVERABLE_ERROR' => 4096,
-			'E_ALL' => 8191
-			);
-		
-		if ( array_key_exists( $level, $errorLevelMap ) )
-			$this->_max_error_reporting_level = $errorLevelMap[ $level ];			
 	}
 	
 	function addOptionsPage() {
